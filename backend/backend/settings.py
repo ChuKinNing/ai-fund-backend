d15952c59ceb2bd8573e310e32ddf7e8566db5fe
Kin Ning Chu <kinningchu624@gmail.com>
2022-04-08 23:26:49 +0800

    off debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['ai-fund-backend.herokuapp', 'localhost', '127.0.0.1', 'ai-fund.herokuapp']
 
